8d176cc1573959c13b6f17090de6c78c2f6a0511
Rodrigo Costanzo <yodri@Rodrigos-MacBook-Pro-2.local>
2020-10-28 20:45:19 -0300

    add mobile stylings

--- src/components/layout/index.js
+++ src/components/layout/index.js
@@ -3,20 +3,35 @@ import styled from 'styled-components';
 const MainWrapper = styled.div`
   display: block;
   margin: 0 auto;
-  padding-top: 80px;
+  padding-top: 40px;
+  width: 85vw;
   max-width: 960px;
+
+  @media screen and (min-width: 768px) {
+    padding-top: 80px
+  }
 `;
 
 const HeroContainer = styled.div`
   display: block;
-  margin-top: 85px;
+  margin-top: 90px;
 `
 
 const SkillsContainer = styled.div`
-  margin: 110px 0;
+  margin: 110px 0 70px;
   display: grid;
-  grid-template-columns: repeat(3, minmax(0, 200px));
-  grid-gap: 70px;
+  grid-template-columns: repeat(2, minmax(0, 200px));
+  grid-gap: 10px 30px;
+
+  @media screen and (min-width: 768px) {
+    grid-template-columns: repeat(3, minmax(0, 200px));
+    grid-gap: 0 60px;
+  }
+
+  @media screen and (min-width: 1024px) {
+    margin: 110px;
+    grid-gap: 0 70px;
+  }
 `
 
 const ExperiencesContainer = styled.div`
@@ -26,12 +41,20 @@ const ExperiencesContainer = styled.div`
 
 const ResumeBannerContainer = styled.div`
   display: block;
-  margin: 90px 0 60px;
+  margin: 50px 0;
+
+  @media screen and (min-width: 768px) {
+    margin: 90px 0 60px;
+  }
 `
 
 const SocialContainer = styled.div`
   display: block;
-  margin-bottom: 110px;
+  margin-bottom: 50px;
+
+  @media screen and (min-width: 768px) {
+    margin-bottom: 110px;
+  }
 `
 
 const Layout = ({header, hero, skills, experiences, resumeBanner, social, footer}) => (
